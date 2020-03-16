a0bf7fc03af4a26cf18c494bb61f907486a3b494
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-16 18:30:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
 # Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length
+    new << yield(source_array[i])
+    i += 1
+  end
+  new
+end
+
+def reduce(source_array, starting_point = 0)
+  i = 0 
+  total = starting_point
+  while i < source_array.length
+    yield(total += source_array[i])
+    i += 1
+  end
+  total
+end
